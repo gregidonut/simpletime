977605c228ddeb6f6bc42c53afc68e58cd9e9d73
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-11 01:29:09 +0800

    feat: add kitchen time
    this string updates every second showing kitchen time with seconds

--- main.go
+++ main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"time"
 )
+
+const WINDOW_TITLE = "Clock"
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow(WINDOW_TITLE)
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	kitchenTime := widget.NewLabel("")
+
+	updatingTime(kitchenTime)
+
+	w.SetContent(kitchenTime)
+
+	go func() {
+		for range time.Tick(1000 * time.Millisecond) {
+			updatingTime(kitchenTime)
+		}
+	}()
 
 	w.ShowAndRun()
+}
+
+func updatingTime(clock *widget.Label) {
+	format := "3:04:05 PM"
+	formatted := time.Now().Format(format)
+
+	clock.SetText(formatted)
 }
